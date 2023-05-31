4cf5c576c41b31f8e3a3642f12558e3e0592dd40
Javi2211 <127950367+Javi2211@users.noreply.github.com>
2023-05-31 12:41:59 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,24 @@
-let velocidad = 50
-let tiempo_de_espera = 1500
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
-basic.pause(velocidad)
-maqueen.motorStop(maqueen.Motors.All)
-maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velocidad)
-basic.pause(tiempo_de_espera)
+let velocidad = 30
+let tiempo_de_espera = 1000
 maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
 basic.pause(tiempo_de_espera)
-maqueen.motorStop(maqueen.Motors.All)
+maqueen.motorStop(maqueen.Motors.M2)
 maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
 basic.pause(tiempo_de_espera)
 maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
-basic.pause(tiempo_de_espera)
-maqueen.motorStop(maqueen.Motors.All)
+basic.pause(1250)
+maqueen.motorStop(maqueen.Motors.M1)
 maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velocidad)
+basic.pause(1500)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
 basic.pause(tiempo_de_espera)
 maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
-basic.pause(velocidad)
+basic.pause(tiempo_de_espera)
+maqueen.motorStop(maqueen.Motors.M2)
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
+basic.pause(1000)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
+basic.pause(500)
+basic.forever(function () {
+	
+})
